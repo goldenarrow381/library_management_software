81c117ded8e9ad2c5b72b304c908c76c4420334c
Suraj Kumar <surajumang08@gmail.com>
2017-02-14 21:37:34 +0530

    initial commit

---
+++ src/Members.java
@@ -0,0 +1,27 @@
+/*
+This contains the details of all the members of the
+library. A student as well as a teacher can be a member.
+Members will be provided with a unique ID.
+Teacher members will be provided additional facilities
+like a maximum of five books can be issued for a period
+of 30 days.
+For student members a maximum of three books can be
+issued for a maximum period of 15 days.
+
+*/
+
+class Members
+{
+  //member is either a student or a Teacher
+  Students studentMember;
+  Teacher teacherMember;
+
+  Members(){
+
+  }
+
+  Members(){
+    
+  }
+
+}
